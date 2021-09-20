19b9d86083eb68cde7f761b65c42e50e2f695097
NHarris02 <Nharris091402@gmail.com>
2021-09-20 14:15:26 -0500

    yeah

---
+++ app.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+let btn = document.createElement("button")
+let text = document.createTextNode("Add Square")
+btn.className = "some button"
+btn.appendChild(text)
+document.body.appendChild(btn)
+
+function click () {
+    
+}
+
+
+
+})
